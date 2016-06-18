0a1cab28eb1384f27e078fb82a047908f705a532
Dennis Kehrig <Dennis Kehrig>
2016-06-18 14:41:21 -0700

    Initial commit based on copy of tutorial 240 (with modifications)

---
+++ Assets/Scripts/RecognitionStatus.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using HoloToolkit.Unity;
+
+public class RecognitionStatus : Singleton<RecognitionStatus>
+{
+    public GameObject recognitionLabel;
+
+    TextMesh content;
+
+    // Use this for initialization
+    void Start () {
+        content = recognitionLabel.GetComponent<TextMesh>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        RaycastHit hitInfo = GazeManager.Instance.HitInfo;
+        content.text = hitInfo.point.x + ", " + hitInfo.point.y + ", " + hitInfo.point.z;
+    }
+}
---
+++ Assets/Scripts/UnitSpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using HoloToolkit.Sharing;
+
+public class UnitSpawner : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnSelect()
+    {
+        Debug.Log("Would spawn");
+    }
+}
