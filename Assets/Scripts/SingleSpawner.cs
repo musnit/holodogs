5d549f153b8ab80212ed16a4b97148ebf46e7013
Aidan Musnitzky <musnit@gmail.com>
2016-06-18 22:03:24 -0700

    Procedural Terrain spawning

---
+++ Assets/Scripts/SingleSpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingleSpawner : MonoBehaviour {
+
+    public GameObject objectPrefab;
+    public Vector3 scaleVector;
+
+    [HideInInspector]
+    public GameObject spawnedObject;
+
+    // Use this for initialization
+    void Start () {
+        spawnedObject = (GameObject)Instantiate(objectPrefab);
+        spawnedObject.transform.position = transform.position;
+        spawnedObject.transform.localScale = scaleVector;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
